8c0707c8b620ba30b539a045f5411e4b5d821255
dhanujadhav1 <dhananjayjadhav2151@gmail.com>
2022-12-26 15:14:24 +0530

    Functional Programming

--- python/functional_programming.py
+++ python/functional_programming.py
@@ -42,16 +42,71 @@ print( sorted( d.items(), key= lambda kv :kv[1]))
 
 # map() -  It applies a function to each element in a sequence like list and tuple and return a new sequence containing results.
 # ex - finding square root of each element of the sequence
+# syntax -  map ( fun_to_apply, list_of_inputs)
+# It returns a map object 
 
+def fun(n):
+    return n*n
 
+lst_temp1 = [1,2,3,4,5,6,7]
+
+m1= map(fun,lst_temp1)
+
+print(list(m1))
+
+import math
+
+m2 = map(math.factorial,lst_temp1)
+print(list(m2))
 
 # filter() - A filter operation applies to all the elements of a sequence and a sequence of those elemets for which the function returns true.
 # ex - checking whether each element in a list is an alphabet and return a list of alphabet.
+# syntax - filter(function_to_apply, list_of_inputs)
 
+def fun(n):
+    if n%5==0:
+        return True
+    else:
+        return False
+
+lst1 = [5,10,18,27,25]
+
+f2= filter(fun,lst1)
+
+print(list(f2))
+
+lst2 = ['A','X','Y','3','M','4','D']
+
+f3 = filter(str.isalpha,lst2)
+
+print(list(f3))
 
 
 #reduce() - a reduce operation performs a rolling computation to sequential pair of values in a sequence and returns the results.
 # ex - obtaining a product of a list of integers and returning the product. Concatenating all string in a list and returning the final string
+# syntax - reduce(fun_to_apply,list_of_inputs)
+
+def get_sum(x,y):
+    return x+y
+
+def get_prod(x,y):
+    return x*y
+
+lst3=[2,3,4,5,6]
+
+from functools import reduce
+reduce(get_sum,[2,3,4,5,6])
+
+reduce(get_prod,lst3)
+
+
+# Functional programming using lambda
+
+list(map( lambda a: a+2,[1,2,3]))
+list(filter( lambda a: a%2==0,[1,2,3,4,5,6,7,8,9,10]))
+
+temp= reduce( lambda a,b:a+b,lst3)
+print(temp)
 
 
 
