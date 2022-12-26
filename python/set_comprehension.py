8f0beefd6f4fea1ba80c1bf269a726b52a00b3c9
dhanujadhav1 <dhananjayjadhav2151@gmail.com>
2022-12-26 07:53:25 +0530

    Set,Dict comprehension and Functions

---
+++ python/set_comprehension.py
@@ -0,0 +1,11 @@
+# print( {a**2 for a in range(10)})
+
+# print( { a for a in range(1,10) if a%2==0 })
+
+# print( { a if a % 2==0 else 'True' for a in range(1,20)})
+
+print( { a for a in range(1,20)  if a % 2==0 })
+
+a='DJ'
+print(a)
+
