05c88892b00cc11fa99741498793751c0330550b
dhanujadhav1 <dhananjayjadhav2151@gmail.com>
2022-12-28 11:10:01 +0530

    Exception handling

--- python/exception_handling.py
+++ python/exception_handling.py
@@ -15,3 +15,44 @@ try :
     print("c=",c)
 except ZeroDivisionError:
     print("Denominator is zero")
+
+# When no exception occures while executing try block control goas to first line beyond the except block
+# and if exception occures during execution of statements in try block an exception is raised and rest of the try block is skipped 
+# after except  control goas to the next line after except block
+
+try : 
+    # some statement 
+    pass
+except(NameError,TypeError,ZeroDivisionError):
+    pass
+
+# or 
+
+try:
+    # some statement 
+    pass
+except(NameError):
+    print("Name Error")
+except(ValueError):
+    print("Unable to convert string into int")
+except:
+    print("Some unknown error")
+# In above it tries to match exception and execute perticular except block 
+
+# user- defined exceptions
+
+# else block (optional)
+# if else block is present in try except block then it is executed if no exception occurs during execution of the try block
+# and it is written after all except blocks
+try:
+    pass
+except:
+    pass
+else:
+    pass
+
+# finally block 
+# this block is optional - code in this block always runs no matter what. Even if return or break occures first.
+# It is placed after except block (if exist)
+# try block must have except block and/or finally block
+# this block is used for releasing external resources like databse connections, network connections.
